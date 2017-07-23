8ca3ba057ca95f0bd0a582d59d77984637943323
aditidevops <aditi.thakur22@gmail.com>
2017-07-23 11:27:39 +0530

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,6 +1,6 @@
 job('DSL-Tutorial-1-Test') {
    scm {
-       git('git://github.com/quidryan/aws-sdk-test.git')
+       git('https://github.com/aditidevops/JenkinsPipelineTesting.git')
    }
    triggers {
        scm('H/15 * * * *')
